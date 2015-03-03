279b45a6607a7a9c742e71774e019e7ebd26740e
dagerebe <rebeccadagenais@Rebeccas-MacBook-Pro-3.local>
2015-03-02 19:12:22 -0500

    starting to style the list

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@ var $form = $('form');
 var $input = $('#to-do');
 
 $form.on('submit', function (eventObject) {
-	// stop the form from doing what it usually does
 	eventObject.preventDefault();
 
-	var $item = $('<h1>');
+	var $items = $('<ul>');
+	$items.html($input.val());
+
+	var $item = $('<li>');
 	$item.html($input.val());
 
 	$theList.html($item);
+	$.append;
 
 	$input.val('');
 });
